ec01b10caa3b2cb6ce2917fbb9b3ac8726616dc2
LAPTOP-NBI9HOC1\juhyu <ju2136@naver.com>
2022-06-23 18:37:31 +0900

    secrets.json

--- poli/settings.py
+++ poli/settings.py
@@ -22,10 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7cctp+kt67&^nw3z-(@xnrx+az)c6fx@!_zfk$11^f^w!9yfms'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
-ALLOWED_HOSTS = []
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 
 # Application definition
