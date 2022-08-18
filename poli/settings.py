54845eb8d534ff66aaa3891a995449baef41cc3d
yoojin park <gina0225@naver.com>
2022-08-18 19:30:04 +0900

    allowed host 변경

--- poli/settings.py
+++ poli/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-7cctp+kt67&^nw3z-(@xnrx+az)c6fx@!_zfk$11^f^w!9yfms
 # SECRET_KEY = get_secret("SECRET_KEY")
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['15.164.212.79']
 
 
 # Application definition
