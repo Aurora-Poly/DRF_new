01fcd73b82c7e593c809e3145273099985568936
LAPTOP-NBI9HOC1\juhyu <ju2136@naver.com>
2022-11-24 13:07:09 +0900

    resume 수정

--- resume/models.py
+++ resume/models.py
@@ -2,14 +2,15 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 from users.models import Profile
 # Create your models here.
-
 class Resume(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume')
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
 
   title = models.CharField(max_length=100) #제목
   file_upload = models.FileField(upload_to='resume/file/', blank=True) #파일
-  date = models.DateField(blank=True, default=datetime.now) #날짜
+  date = models.DateField(blank=True,  auto_now_add=True) #날짜
   comments = models.TextField() #코멘트
